2c773c27f97a3dc8e6ee79461a44c7dfe92ee42a
maulaniad <maulani.ad@gmail.com>
2023-11-10 00:17:46 +0700

    Buat model tabel

---
+++ app/Models/TB_Barang_Retur.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TB_Barang_Retur extends Model
+{
+    use HasFactory;
+}
